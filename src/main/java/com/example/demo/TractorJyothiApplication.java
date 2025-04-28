c2e2768e7daea5069d86074d11d05677b8ea8a16
Manjula Nannuri <manjulanannuri11@gmail.com>
2025-04-28 17:28:16 +0530

    inti  Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/example/demo/TractorJyothiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TractorJyothiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TractorJyothiApplication.class, args);
+	}
+
+}
